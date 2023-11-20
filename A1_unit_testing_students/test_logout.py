c74463731e36c2b9cfaefb68ef4b6291df10bd0d
Tjasomfan <73586986+Tjasomfan@users.noreply.github.com>
2023-11-20 16:00:41 +0100

    Create test_logout.py

---
+++ A1_unit_testing_students/test_logout.py
@@ -0,0 +1,17 @@
+# Test return for cart with 0 items
+
+# Test output for cart with 1 item
+
+# Test output for cart with 3 items
+
+# Test return for cart with 1 item and input "Y"
+
+# Test return for cart with 1 item and input "N"
+
+# Test return for cart with 1 item and input "y"
+
+# Test return for cart with 1 item and input "n"
+
+# Test length of content of cart with 1 item and input "Y"
+
+# Test length of content of cart with 1 item and input "N"
