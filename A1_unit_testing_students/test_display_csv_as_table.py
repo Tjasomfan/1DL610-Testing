6422974551a62f8dcce0522f2801e5e26c0da983
Tjasomfan <isac.arrhenius@gmail.com>
2023-11-21 12:30:20 +0100

    Create test_display_csv_as_table.py

---
+++ A1_unit_testing_students/test_display_csv_as_table.py
@@ -0,0 +1,19 @@
+# Test with an empty .csv file
+
+# Test with a .csv file with only a header
+
+# Test with a .csv file with a header and one row
+
+# Test with a .csv file with no header and one row
+
+# Test with a .csv file with a header and several rows
+
+# Test with a .txt file containing a header and a row
+
+# Test with a .txt file containing no header and a row
+
+# Test with a .txt file containing a header and no rows
+
+# Test with a string that is not a file
+
+# Test with an empty string
