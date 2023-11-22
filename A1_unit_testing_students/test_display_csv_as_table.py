082fc713663ed86b2ee22af90074202d7d112b46
Tjasomfan <isac.arrhenius@gmail.com>
2023-11-22 19:23:54 +0100

    Update test_display_csv_as_table.py

--- A1_unit_testing_students/test_display_csv_as_table.py
+++ A1_unit_testing_students/test_display_csv_as_table.py
@@ -17,3 +17,70 @@
 # Test with a string that is not a file
 
 # Test with an empty string
+
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from products import display_csv_as_table
+
+class TestDisplayCSVAsTable(unittest.TestCase):
+
+    def test_empty_csv_file(self):
+        with patch('builtins.open', return_value=StringIO()) as mock_open:
+            try:
+                result = display_csv_as_table('empty.csv')
+                self.assertIsNone(result)
+            except:
+                print("Error caught")
+
+    def test_csv_file_with_only_header(self):
+        csv_content = 'header1,header2,header3\n'
+        with patch('builtins.open', return_value=StringIO(csv_content)) as mock_open:
+            result = display_csv_as_table('header_only.csv')
+            self.assertIsNone(result)
+
+    def test_csv_file_with_header_and_one_row(self):
+        csv_content = 'header1,header2,header3\nvalue1,value2,value3\n'
+        with patch('builtins.open', return_value=StringIO(csv_content)) as mock_open:
+            result = display_csv_as_table('header_and_one_row.csv')
+            self.assertIsNone(result)
+
+    def test_csv_file_no_header_one_row(self):
+        csv_content = 'value1,value2,value3\n'
+        with patch('builtins.open', return_value=StringIO(csv_content)) as mock_open:
+            result = display_csv_as_table('no_header_one_row.csv')
+            self.assertIsNone(result)
+
+    def test_csv_file_with_header_and_several_rows(self):
+        csv_content = 'header1,header2,header3\nvalue1,value2,value3\nvalue4,value5,value6\n'
+        with patch('builtins.open', return_value=StringIO(csv_content)) as mock_open:
+            result = display_csv_as_table('header_and_several_rows.csv')
+            self.assertIsNone(result)
+
+    def test_txt_file_with_header_and_row(self):
+        txt_content = 'header1,header2,header3\nvalue1,value2,value3\n'
+        with patch('builtins.open', return_value=StringIO(txt_content)) as mock_open:
+            result = display_csv_as_table('header_and_row.txt')
+            self.assertIsNone(result)
+
+    def test_txt_file_no_header_one_row(self):
+        txt_content = 'value1,value2,value3\n'
+        with patch('builtins.open', return_value=StringIO(txt_content)) as mock_open:
+            result = display_csv_as_table('no_header_one_row.txt')
+            self.assertIsNone(result)
+
+    def test_txt_file_with_header_no_rows(self):
+        txt_content = 'header1,header2,header3\n'
+        with patch('builtins.open', return_value=StringIO(txt_content)) as mock_open:
+            result = display_csv_as_table('header_no_rows.txt')
+            self.assertIsNone(result)
+
+    def test_non_file_string(self):
+        with patch('builtins.open', return_value=StringIO()) as mock_open:
+            with self.assertRaises(TypeError):
+                display_csv_as_table('not_a_file')
+
+    def test_empty_string(self):
+        with patch('builtins.open', return_value=StringIO()) as mock_open:
+            with self.assertRaises(ValueError):
+                display_csv_as_table('')
