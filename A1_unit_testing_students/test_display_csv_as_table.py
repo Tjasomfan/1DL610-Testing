c2ee1627cff0d089f03b9b12a553fcc86fcf4dec
Tjasomfan <isac.arrhenius@gmail.com>
2023-12-15 10:25:14 +0100

    Update test_display_csv_as_table.py

--- A1_unit_testing_students/test_display_csv_as_table.py
+++ A1_unit_testing_students/test_display_csv_as_table.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 from unittest.mock import patch
 import tempfile
 from io import StringIO
@@ -10,171 +10,106 @@ def create_temporary_file(content='', _suffix='.csv'):
     temp_file.close()
     return temp_file
 
-class TestDisplayCSVAsTable(unittest.TestCase):
+def test_empty_csv_file():
+    csv_content = ''
+    temp_file = create_temporary_file(csv_content, '.csv')
+    with pytest.raises(Exception):
+        display_csv_as_table(temp_file.name)
 
-    def test_empty_csv_file(self):
-        csv_content = ''
-        # Create a temporary CSV file
-        temp_file = create_temporary_file(csv_content, '.csv')
-        result = None
-        
-        try:
-            result = display_csv_as_table(temp_file.name)
-            assert False
-        except:
-            assert True
-            self.assertIsNone(result)
+def test_csv_file_with_only_header():
+    csv_content = 'header1,header2,header3\n'
+    temp_file = create_temporary_file(csv_content, '.csv')
+    with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
+        display_csv_as_table(temp_file.name)
+        mock_stdout.seek(0)
+        captured_output = mock_stdout.read()
+    expected_output = "['header1', 'header2', 'header3']\n"
+    assert captured_output == expected_output
 
-    def test_csv_file_with_only_header(self):
-        csv_content = 'header1,header2,header3\n'
-        # Create a temporary CSV file
-        temp_file = create_temporary_file(csv_content, '.csv')
+def test_csv_file_with_header_and_one_row():
+    csv_content = 'header1,header2,header3\nvalue1,value2,value3\n'
+    temp_file = create_temporary_file(csv_content, '.csv')
+    with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
+        display_csv_as_table(temp_file.name)
+        mock_stdout.seek(0)
+        captured_output = mock_stdout.read()
+    expected_output = "['header1', 'header2', 'header3']\n['value1', 'value2', 'value3']\n"
+    assert captured_output == expected_output
 
-        # Test function with mock stdout
-        with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
-            display_csv_as_table(temp_file.name)
-            mock_stdout.seek(0)
-            captured_output = mock_stdout.read()
+def test_csv_file_no_header_one_row():
+    csv_content = 'value1,value2,value3\n'
+    temp_file = create_temporary_file(csv_content, '.csv')
+    with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
+        display_csv_as_table(temp_file.name)
+        mock_stdout.seek(0)
+        captured_output = mock_stdout.read()
+    expected_output = "['value1', 'value2', 'value3']\n"
+    assert captured_output == expected_output
 
-        expected_output = "['header1', 'header2', 'header3']\n"
-        self.assertEqual(captured_output, expected_output)
+def test_csv_file_with_header_and_several_rows():
+    csv_content = 'header1,header2,header3\nvalue1,value2,value3\nvalue4,value5,value6\n'
+    temp_file = create_temporary_file(csv_content, '.csv')
+    with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
+        display_csv_as_table(temp_file.name)
+        mock_stdout.seek(0)
+        captured_output = mock_stdout.read()
+    expected_output = "['header1', 'header2', 'header3']\n['value1', 'value2', 'value3']\n['value4', 'value5', 'value6']\n"
+    assert captured_output == expected_output
 
-    def test_csv_file_with_header_and_one_row(self):
-        csv_content = 'header1,header2,header3\nvalue1,value2,value3\n'
-        # Create a temporary CSV file
-        temp_file = create_temporary_file(csv_content, '.csv')
+def test_txt_file_with_header_and_row():
+    txt_content = 'header1,header2,header3\nvalue1,value2,value3\n'
+    temp_file = create_temporary_file(txt_content, '.txt')
+    with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
+        display_csv_as_table(temp_file.name)
+        mock_stdout.seek(0)
+        captured_output = mock_stdout.read()
+    expected_output = "['header1', 'header2', 'header3']\n['value1', 'value2', 'value3']\n"
+    assert captured_output == expected_output
 
-        # Test function with mock stdout
-        with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
-            display_csv_as_table(temp_file.name)
-            mock_stdout.seek(0)
-            captured_output = mock_stdout.read()
+def test_txt_file_no_header_one_row():
+    txt_content = 'value1,value2,value3\n'
+    temp_file = create_temporary_file(txt_content, '.txt')
+    with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
+        display_csv_as_table(temp_file.name)
+        mock_stdout.seek(0)
+        captured_output = mock_stdout.read()
+    expected_output = "['value1', 'value2', 'value3']\n"
+    assert captured_output == expected_output
 
-        expected_output = "['header1', 'header2', 'header3']\n['value1', 'value2', 'value3']\n"
-        self.assertEqual(captured_output, expected_output)
+def test_txt_file_with_header_no_rows():
+    txt_content = 'header1,header2,header3\n'
+    temp_file = create_temporary_file(txt_content, '.txt')
+    with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
+        display_csv_as_table(temp_file.name)
+        mock_stdout.seek(0)
+        captured_output = mock_stdout.read()
+    expected_output = "['header1', 'header2', 'header3']\n"
+    assert captured_output == expected_output
 
-    def test_csv_file_no_header_one_row(self):
-        csv_content = 'value1,value2,value3\n'
-        # Create a temporary CSV file
-        temp_file = create_temporary_file(csv_content, '.csv')
+def test_non_file_string():
+    with patch('builtins.open', return_value=StringIO()):
+        with pytest.raises(Exception):
+            display_csv_as_table('not_a_file')
 
-        # Test function with mock stdout
-        with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
-            display_csv_as_table(temp_file.name)
-            mock_stdout.seek(0)
-            captured_output = mock_stdout.read()
+def test_empty_string():
+    with patch('builtins.open', return_value=StringIO()):
+        with pytest.raises(Exception):
+            display_csv_as_table('')
 
-        expected_output = "['value1', 'value2', 'value3']\n"
-        self.assertEqual(captured_output, expected_output)
+def test_int_input():
+    csv_content = ''
+    temp_file = create_temporary_file(csv_content, '.csv')
+    with pytest.raises(Exception):
+        display_csv_as_table(15)
 
-    def test_csv_file_with_header_and_several_rows(self):
-        csv_content = 'header1,header2,header3\nvalue1,value2,value3\nvalue4,value5,value6\n'
-        # Create a temporary CSV file
-        temp_file = create_temporary_file(csv_content, '.csv')
+def test_float_input():
+    csv_content = ''
+    temp_file = create_temporary_file(csv_content, '.csv')
+    with pytest.raises(Exception):
+        display_csv_as_table(5003.5)
 
-        # Test function with mock stdout
-        with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
-            display_csv_as_table(temp_file.name)
-            mock_stdout.seek(0)
-            captured_output = mock_stdout.read()
-
-        expected_output = "['header1', 'header2', 'header3']\n['value1', 'value2', 'value3']\n['value4', 'value5', 'value6']\n"
-        self.assertEqual(captured_output, expected_output)
-
-    def test_txt_file_with_header_and_row(self):
-        txt_content = 'header1,header2,header3\nvalue1,value2,value3\n'
-        # Create a temporary TXT file
-        temp_file = create_temporary_file(txt_content, '.txt')
-
-        # Test function with mock stdout
-        with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
-            display_csv_as_table(temp_file.name)
-            mock_stdout.seek(0)
-            captured_output = mock_stdout.read()
-
-        expected_output = "['header1', 'header2', 'header3']\n['value1', 'value2', 'value3']\n"
-        self.assertEqual(captured_output, expected_output)
-
-    def test_txt_file_no_header_one_row(self):
-        txt_content = 'value1,value2,value3\n'
-        # Create a temporary TXT file
-        temp_file = create_temporary_file(txt_content, '.txt')
-
-        # Test function with mock stdout
-        with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
-            display_csv_as_table(temp_file.name)
-            mock_stdout.seek(0)
-            captured_output = mock_stdout.read()
-
-        expected_output = "['value1', 'value2', 'value3']\n"
-        self.assertEqual(captured_output, expected_output)
-
-    def test_txt_file_with_header_no_rows(self):
-        txt_content = 'header1,header2,header3\n'
-        # Create a temporary TXT file
-        temp_file = create_temporary_file(txt_content, '.txt')
-
-        # Test function with mock stdout
-        with patch('sys.stdout', new_callable=tempfile.SpooledTemporaryFile, mode='w+t', create=True) as mock_stdout:
-            display_csv_as_table(temp_file.name)
-            mock_stdout.seek(0)
-            captured_output = mock_stdout.read()
-
-        expected_output = "['header1', 'header2', 'header3']\n"
-        self.assertEqual(captured_output, expected_output)
-
-    def test_non_file_string(self):
-        with patch('builtins.open', return_value=StringIO()) as mock_open:
-            try:
-                display_csv_as_table('not_a_file')
-                assert False
-            except:
-                assert True
-
-    def test_empty_string(self):
-        with patch('builtins.open', return_value=StringIO()) as mock_open:
-            try:
-                display_csv_as_table('')
-                assert False
-            except:
-                assert True
-
-    def test_int_input(self):
-        csv_content = ''
-        # Create a temporary CSV file
-        temp_file = create_temporary_file(csv_content, '.csv')
-        result = None
-        
-        try:
-            result = display_csv_as_table(15)
-            assert False
-        except:
-            assert True
-            self.assertIsNone(result)
-
-    def test_float_input(self):
-        csv_content = ''
-        # Create a temporary CSV file
-        temp_file = create_temporary_file(csv_content, '.csv')
-        result = None
-        
-        try:
-            result = display_csv_as_table(5003.5)
-            assert False
-        except:
-            assert True
-            self.assertIsNone(result)
-
-    def test_list_input(self):
-        csv_content = ''
-        # Create a temporary CSV file
-        temp_file = create_temporary_file(csv_content, '.csv')
-        result = None
-        
-        try:
-            result = display_csv_as_table([temp_file.name, "hello.csv", "products.csv", "login.py"])
-            assert False
-        except:
-            assert True
-            self.assertIsNone(result)
-
+def test_list_input():
+    csv_content = ''
+    temp_file = create_temporary_file(csv_content, '.csv')
+    with pytest.raises(Exception):
+        display_csv_as_table([temp_file.name, "hello.csv", "products.csv", "login.py"])
